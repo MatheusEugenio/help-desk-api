c44345fd846a1e9c2a399bac1909519e1c033f19
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-06 18:10:10 -0300

    feat: add new entities, Categoria and HistoricoChamado

--- src/main/java/com/database/model/Categoria.java
+++ src/main/java/com/database/model/Categoria.java
@@ -1,4 +1,27 @@
 package com.database.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 50, name = "nome_categoria")
+    private String nomeCategoria;
+
+    @Column
+    private Boolean ativa;
 }
--- src/main/java/com/database/model/HistoricoChamadoModel.java
+++ src/main/java/com/database/model/HistoricoChamadoModel.java
@@ -1,4 +1,41 @@
 package com.database.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "historico_chamado")
 public class HistoricoChamadoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 30)
+    private String tipoAlteracao;
+
+    @Column(length = 40)
+    private String valorAnterior;
+
+    @Column(length = 40)
+    private String novoValor;
+
+    @ManyToOne
+    @JoinColumn(name = "chamado_id", nullable = false)
+    @NotNull
+    private ChamadoModel chamado;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    @NotNull
+    private UsuarioModel autor;
 }
