eb526476748ed36d06553f1fac5e8e848f62a5e0
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-06 18:14:51 -0300

    style: rename method NotFoundHandler to NotFounfExceptionHandler

--- src/main/java/com/handler/GlobalExceptionHandler.java
+++ src/main/java/com/handler/GlobalExceptionHandler.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorReponse> NotFoundHandler(NotFoundException ex) {
-
+    public ResponseEntity<ErrorReponse> NotFounExceptiondHandler(NotFoundException ex) {
         ErrorReponse errorReponse = ErrorReponse.builder()
                 .mensagem(ex.getMessage())
-                .status(404)
+                .status(HttpStatus.NOT_FOUND.value())
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(errorReponse);
