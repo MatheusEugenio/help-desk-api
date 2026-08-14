e5748e40a1a950bce061c955c9c4c19e7480ab36
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-14 20:53:08 -0300

    feat: add IHistoricoChamadoRepository

--- src/main/java/com/database/repository/IHistoricoChamadoRepository.java
+++ src/main/java/com/database/repository/IHistoricoChamadoRepository.java
@@ -1,4 +1,7 @@
 package com.database.repository;
 
-public interface IHistoricoChamadoRepository {
+import com.database.model.HistoricoChamadoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IHistoricoChamadoRepository extends JpaRepository<HistoricoChamadoModel, Long> {
 }
