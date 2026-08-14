9748d887dbd489979f317a51d9423a4096722373
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-14 20:46:14 -0300

    feat: add annotations

--- src/main/java/com/dto/ChamadoDTO.java
+++ src/main/java/com/dto/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.dto;
 import com.database.enums.PrioridadeEnum;
 import com.database.model.UsuarioModel;
 import com.database.enums.StatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -13,8 +15,15 @@ import lombok.*;
 @Builder
 public class ChamadoDTO {
 
+    @NotBlank
     private String titulo;
+
+    @NotNull
     private StatusEnum status;
+
+    @NotNull
     private PrioridadeEnum prioridade;
+
+    @NotNull
     private UsuarioModel solicitante;
 }
