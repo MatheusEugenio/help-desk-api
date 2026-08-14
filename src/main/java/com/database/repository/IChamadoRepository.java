166899af5b32004bbe78aaedf8b8d64ae9ef72eb
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-14 20:52:40 -0300

    feat: add query methods in IChamadoRepository

--- src/main/java/com/database/repository/IChamadoRepository.java
+++ src/main/java/com/database/repository/IChamadoRepository.java
@@ -3,5 +3,10 @@ package com.database.repository;
 import com.database.model.ChamadoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IChamadoRepository extends JpaRepository<ChamadoModel, Long> {
+
+    Optional<ChamadoModel> findByTitulo(String titulo);
+
 }
