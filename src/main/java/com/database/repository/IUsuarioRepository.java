b22bc2a6f4e0afaf2678be4768843e444a53ee6b
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-18 15:51:26 -0300

    feat: create a query method to search for a user by email in UsuarioRepository

--- src/main/java/com/database/repository/IUsuarioRepository.java
+++ src/main/java/com/database/repository/IUsuarioRepository.java
@@ -3,5 +3,9 @@ package com.database.repository;
 import com.database.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+    Optional<UsuarioModel> findByEmail(String email);
 }
