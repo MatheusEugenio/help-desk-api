f07f55cbbd40f85eef16f8d03a4ad0314d3780e2
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-20 18:37:22 -0300

    style: change the path of the controllers in the plural

--- src/main/java/com/controller/ChamadoController.java
+++ src/main/java/com/controller/ChamadoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/chamado")
+@RequestMapping("/v1/chamados")
 @RequiredArgsConstructor
 @Validated
 public class ChamadoController {
--- src/main/java/com/controller/UsuarioController.java
+++ src/main/java/com/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/usuario")
+@RequestMapping("v1/usuarios")
 @Validated
 @RequiredArgsConstructor
 public class UsuarioController {
