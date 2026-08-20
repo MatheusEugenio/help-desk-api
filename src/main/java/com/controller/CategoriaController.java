c18c2b16e75f17af0f8382f890cd9c5e67b78445
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-20 18:04:34 -0300

    feat: create CategoriaController

---
+++ src/main/java/com/controller/CategoriaController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import com.database.model.Categoria;
+import com.service.CategoriaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/categoria")
+@Validated
+@RequiredArgsConstructor
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Categoria> findAll() {return categoriaService.findAll();}
+
+    @PostMapping("/add/{nomeCategoria}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCategoria(@Valid @PathVariable String nomeCategoria) {
+            categoriaService.addCategoria(nomeCategoria);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCategoria(@Valid @PathVariable Long id){
+        categoriaService.deleteCategoria(id);
+    }
+}
