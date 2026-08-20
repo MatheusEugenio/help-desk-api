aaffc51b921ebb416a45a1e7597caf2d424e4db6
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-20 18:54:57 -0300

    feat: add new method findByID in CategoriaService.java and CategoriaController

--- src/main/java/com/controller/CategoriaController.java
+++ src/main/java/com/controller/CategoriaController.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 import com.database.model.Categoria;
+import com.dto.ResponseChamadoDTO;
+import com.exception.NotFoundException;
 import com.service.CategoriaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,12 @@ public class CategoriaController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Categoria> findAll() {return categoriaService.findAll();}
+
+    @GetMapping("/id-chamado/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Categoria viewChamadoByID(@Valid @PathVariable Long id) throws NotFoundException {
+        return categoriaService.findById(id);
+    }
 
     @PostMapping("/add/{nomeCategoria}")
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/service/CategoriaService.java
+++ src/main/java/com/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.service;
 
 import com.database.model.Categoria;
 import com.database.repository.ICategoriaRepository;
+import com.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,17 @@ public class CategoriaService {
     @Transactional(rollbackFor = Exception.class)
     public void deleteCategoria(Long id) {
         categoriaRepository.deleteById(id);
+    }
+
+    public Categoria findById(Long id) throws NotFoundException {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElse(null);
+
+        if  (categoria == null) {
+            throw new NotFoundException("Categoria com id ="+id+" não encontrado");
+        }
+
+        return categoria;
     }
 
     public void addCategoria(String nomeCategoria)  {
