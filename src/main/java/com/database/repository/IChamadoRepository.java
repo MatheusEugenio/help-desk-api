365ca2ee99dc31164d36338bbf6572c7993fe990
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-22 14:04:35 -0300

    feat(ICHamadoRepository): add JpaSpecificationExecutor dependency

--- src/main/java/com/database/repository/IChamadoRepository.java
+++ src/main/java/com/database/repository/IChamadoRepository.java
@@ -1,12 +1,12 @@
 package com.database.repository;
 
 import com.database.model.ChamadoModel;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface IChamadoRepository extends JpaRepository<ChamadoModel, Long>, Specification<ChamadoModel> {
+public interface IChamadoRepository extends JpaRepository<ChamadoModel, Long>, JpaSpecificationExecutor<ChamadoModel> {
 
     Optional<ChamadoModel> findByTitulo(String titulo);
 
