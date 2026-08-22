b64a98e7b455485ece2fc37a06da9ad07d3e7ef3
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-21 23:19:42 -0300

    feat(IChamadoRepository): extend Specification to add filters

--- src/main/java/com/database/repository/IChamadoRepository.java
+++ src/main/java/com/database/repository/IChamadoRepository.java
@@ -1,11 +1,12 @@
 package com.database.repository;
 
 import com.database.model.ChamadoModel;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface IChamadoRepository extends JpaRepository<ChamadoModel, Long> {
+public interface IChamadoRepository extends JpaRepository<ChamadoModel, Long>, Specification<ChamadoModel> {
 
     Optional<ChamadoModel> findByTitulo(String titulo);
 
