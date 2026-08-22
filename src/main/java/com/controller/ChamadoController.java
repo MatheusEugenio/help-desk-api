54fa69e986bf7f99d79b517c5c2b5b67287ef79a
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-22 14:00:16 -0300

    refactor(ChamadoController): add parameters for query filtering and simplify endpoint routing

--- src/main/java/com/controller/ChamadoController.java
+++ src/main/java/com/controller/ChamadoController.java
@@ -2,7 +2,6 @@ package com.controller;
 
 import com.database.enums.PrioridadeEnum;
 import com.database.enums.StatusEnum;
-import com.database.model.ChamadoModel;
 import com.database.model.HistoricoChamadoModel;
 import com.dto.ChamadoDTO;
 import com.dto.ResponseChamadoDTO;
@@ -29,8 +28,11 @@ public class ChamadoController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ChamadoModel> findAll() {
-        return chamadoService.findAll();
+    public List<ResponseChamadoDTO> findAll(@RequestParam(required = false) StatusEnum status,
+                                            @RequestParam(required = false) PrioridadeEnum prioridade,
+                                            @RequestParam(required = false) Long idCategoria ,
+                                            @RequestParam(required = false) Long idSolicitante) {
+        return chamadoService.findAll(status, prioridade, idCategoria, idSolicitante);
     }
 
     @PostMapping
@@ -39,31 +41,31 @@ public class ChamadoController {
         return chamadoService.createdChamado(chamadoDTO);
     }
 
-    @GetMapping("/id-chamado/{id}")
+    @GetMapping("/{id_chamado}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseChamadoDTO viewChamadoByID(@Valid @PathVariable Long id) throws NotFoundException {
-        return chamadoService.findByID(id);
+    public ResponseChamadoDTO viewChamadoByID(@Valid @PathVariable Long idChamado) throws NotFoundException {
+        return chamadoService.findByID(idChamado);
     }
 
-    @PutMapping("/historico/id-chamado/{id}")
+    @GetMapping("/{id_historico}")
     @ResponseStatus(HttpStatus.OK)
-    public List<HistoricoChamadoModel> historicoChamadoFindById(@Valid @PathVariable Long id) throws NotFoundException {
-        return chamadoService.historicoChamadoFindByID(id);
+    public List<HistoricoChamadoModel> historicoChamadoFindById(@Valid @PathVariable Long idHistorico) throws NotFoundException {
+        return chamadoService.historicoChamadoFindByID(idHistorico);
     }
 
-    @PatchMapping(value = "/id-chamado/{id}/nova-prioridade/{prioridade}")
+    @PatchMapping(value = "/{id_chamado}/{prioridade}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseChamadoDTO updatePrioridadeChamado(@Valid @PathVariable Long id, @Valid @PathVariable PrioridadeEnum prioridade) throws NotFoundException, CallCompletedException, CallInactiveException {
-        return chamadoService.updatePrioridade(id, prioridade);
+    public ResponseChamadoDTO updatePrioridadeChamado(@Valid @PathVariable Long idChamado, @Valid @PathVariable PrioridadeEnum prioridade) throws NotFoundException, CallCompletedException, CallInactiveException {
+        return chamadoService.updatePrioridade(idChamado, prioridade);
     }
 
-    @PatchMapping(value = "/id-chamado/{id}/novo-status/{status}")
+    @PatchMapping(value = "/{id_chamado}/{status}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseChamadoDTO updateStatusChamado(@Valid @PathVariable Long id, @Valid @PathVariable StatusEnum status) throws NotFoundException, CallCompletedException, CallInactiveException {
-        return chamadoService.updateStatus(id, status);
+    public ResponseChamadoDTO updateStatusChamado(@Valid @PathVariable Long idChamado, @Valid @PathVariable StatusEnum status) throws NotFoundException, CallCompletedException, CallInactiveException {
+        return chamadoService.updateStatus(idChamado, status);
     }
 
-    @DeleteMapping(value = "/delete/id-chamado/{id}")
+    @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void inativarChamado(@Valid @PathVariable Long id) throws NotFoundException {
         chamadoService.inativarChamado(id);
