d8de659f1ff39415e4048a6d3d4c919f38032f3e
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-08-26 11:02:54 -0300

    feat(IUsuarioRepository): add dependency on JpaSpecificationExecutor

--- src/main/java/com/database/repository/IUsuarioRepository.java
+++ src/main/java/com/database/repository/IUsuarioRepository.java
@@ -2,10 +2,11 @@ package com.database.repository;
 
 import com.database.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Long>, JpaSpecificationExecutor<UsuarioModel> {
 
     Optional<UsuarioModel> findByEmail(String email);
 }
