a69d0b99aff95601e69a940764594f56cb25dee5
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-09-04 22:08:51 -0300

    feat(CategoriaRepository.java): rename ICategoriaRepository to CategoriaRepository for consistency
    Signed-off-by: MatheusEugenio <eugeniomatheus977@gmail.com>

--- src/main/java/com/database/repository/CategoriaRepository.java
+++ src/main/java/com/database/repository/CategoriaRepository.java
@@ -3,5 +3,5 @@ package com.database.repository;
 import com.database.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICategoriaRepository extends JpaRepository<Categoria, Long> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
